ef58c2d1c49d85a1aa6dab4265cc8a71ed501a77
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-10 15:14:01 +0700

    change view and styling

--- client/App.js
+++ client/App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
-export default function App() {
+import useStyles from './styles.js';
+
+function Container() {
+  const styles = useStyles();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ textAlign: 'center', fontSize: 36, fontWeight: '500' }}>Bismillah</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <Container />
+  );
+}

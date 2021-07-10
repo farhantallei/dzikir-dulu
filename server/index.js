aa0a7f922f48d86859878a1a0d2b81e8277dab0b
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-10 13:02:36 +0700

    Connecting the Server to the Database

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://<username>:<password>@<cluster>.mmgd6.mongodb.net/<database>?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb://farhantallei:419555444Sifrhn@sakudzikr-cluster-0-shard-00-00.mmgd6.mongodb.net:27017,sakudzikr-cluster-0-shard-00-01.mmgd6.mongodb.net:27017,sakudzikr-cluster-0-shard-00-02.mmgd6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-irhbzc-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })

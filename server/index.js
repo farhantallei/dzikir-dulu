78406bfb1be721fdfb0470bfb2193631eb62ab25
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-10 16:55:21 +0700

    Routing the Server

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import collectionRoutes from './routes/collections.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/collections', collectionRoutes);
 
 const CONNECTION_URL = 'mongodb://farhantallei:419555444Sifrhn@sakudzikr-cluster-0-shard-00-00.mmgd6.mongodb.net:27017,sakudzikr-cluster-0-shard-00-01.mmgd6.mongodb.net:27017,sakudzikr-cluster-0-shard-00-02.mmgd6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-irhbzc-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;

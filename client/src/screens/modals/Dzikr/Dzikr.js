06578493fe829341fef748a2bb7ea4d1f3eb5718
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-12 19:37:14 +0700

    separate and naming styles

--- client/src/screens/modals/Dzikr/Dzikr.js
+++ client/src/screens/modals/Dzikr/Dzikr.js
@@ -10,9 +10,11 @@ export default function Dzikr({navigation}) {
     const styles = useStyles();
     const [count, setCount] = useState(0);
     const [progress, setProgress] = useState(0);
+    const [opacity, setOpacity] = useState(0);
     
     useEffect(() => {
         setProgress(count/100);
+        count === 0 ? setOpacity(0) : setOpacity(1);
     }, [count])
 
     const incrementCount = () => {
@@ -30,49 +32,49 @@ export default function Dzikr({navigation}) {
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{ height: 40, flexDirection: 'row' }}>
+            <View style={styles.header}>
                 <MainButton style={{ marginRight: 16 }} width={16} height={16} length={0} backgroundColor='#8e8e93' label='' marginBottom={0} onPress={() => navigation.goBack()} />
-                <ProgressBar progress={progress} />
+                <ProgressBar progress={progress} opacity={opacity} />
             </View>
-            <View style={{ flex: 1, marginBottom: 24 }}>
-                <View style={{ flexDirection: 'row', height: 54, justifyContent: 'center', marginHorizontal: 16 }}>
-                    <View style={{ width: 70 }} />
-                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
-                        <Text style={{ fontSize: 18, fontWeight: '500' }}>baca</Text>
-                        <Text style={{ fontSize: 24, fontWeight: '600' }}>3X</Text>
+            <View style={styles.section}>
+                <View style={styles.detail}>
+                    <View style={styles.leftDetail} />
+                    <View style={styles.centerDetail}>
+                        <Text style={styles.pMed}>baca</Text>
+                        <Text style={styles.h1Sem}>3X</Text>
                     </View>
-                    <View style={{ width: 70, alignItems: 'flex-end', justifyContent: 'center' }}>
+                    <View style={styles.rightDetail}>
                         <Text>
-                            <Text style={{ fontSize: 24, fontWeight: '600' }}>2 </Text>
-                            <Text style={{ fontSize: 18 }}>/ 18</Text>
+                            <Text style={styles.h1Sem}>2 </Text>
+                            <Text style={styles.pNor}>/ 18</Text>
                         </Text>
                     </View>
                 </View>
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', marginTop: 20, paddingHorizontal: 24, paddingVertical: 36, borderRadius: 30, backgroundColor: '#f2f2f7' }}>
+                <View style={styles.card}>
                     <View>
-                        <Text style={{ fontSize: 24, fontWeight: '500', textAlign: 'center', lineHeight: 40, marginBottom: 16, writingDirection: 'rtl' }}>بِسْمِ اللهِ الرَّحْمَنِ الرَّحِيمِ</Text>
-                        <Text style={{ fontSize: 24, fontWeight: '500', textAlign: 'center', lineHeight: 40, marginBottom: 16, writingDirection: 'rtl' }}>قُلْ هُوَ اللهُ أَحَدٌ، اللهُ الصَّمَدُ، لَمْ يَلِدْ وَلَمْ يُولَدْ، وَلَمْ يَكُن لَّهُ كُفُوًا أَحَدٌ</Text>
+                        <Text style={styles.cardArabic}>بِسْمِ اللهِ الرَّحْمَنِ الرَّحِيمِ</Text>
+                        <Text style={styles.cardArabic}>قُلْ هُوَ اللهُ أَحَدٌ، اللهُ الصَّمَدُ، لَمْ يَلِدْ وَلَمْ يُولَدْ، وَلَمْ يَكُن لَّهُ كُفُوًا أَحَدٌ</Text>
                     </View>
-                    <Text style={{ color: '#8E8E93', fontSize: 18, fontWeight: '500' }}>QS. Al-Ikhlas: 1-4</Text>
+                    <Text style={styles.cardLabel}>QS. Al-Ikhlas: 1-4</Text>
                 </View>
             </View>
-            <View style={{ height: 150, justifyContent: 'flex-end' }}>
-                <View style={{ flexDirection: 'row', marginBottom: 15, justifyContent: 'space-between' }}>
-                    <View style={{ flex: 1, alignItems: 'flex-end', justifyContent: 'center', marginRight: 35 }}>
+            <View style={styles.footer}>
+                <View style={styles.moreActionContainer}>
+                    <View style={styles.subtractAction}>
                         <MainButton label='' height={30} width={30} length={4} radius={15} backgroundColor={'orange'} marginBottom={0} onPress={decrementCount} />
                     </View>
-                    <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'center' }}>
+                    <View style={styles.resetAction}>
                         <MainButton label='' height={30} width={30} length={4} radius={15} backgroundColor={'red'} marginBottom={0} onPress={resetCount} />
                     </View>
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <View style={{ flex: .7, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={styles.actionContainer}>
+                    <View style={styles.prevNextAction}>
                         <MainButton label='prev' color={'#8e8e93'} backgroundColor={'#f2f2f7'} marginBottom={0} onPress={() => {}} />
                     </View>
-                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <View style={styles.centerAction}>
                         <CountButton count={count} onPress={incrementCount} />
                     </View>
-                    <View style={{ flex: .7, alignItems: 'center', justifyContent: 'center' }}>
+                    <View style={styles.prevNextAction}>
                         <MainButton label='next' backgroundColor={'#34c759'} marginBottom={0} onPress={() => {}} />
                     </View>
                 </View>
--- client/src/screens/modals/Dzikr/styles.js
+++ client/src/screens/modals/Dzikr/styles.js
@@ -2,10 +2,122 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create(() => ({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      justifyContent: 'space-around',
-      marginHorizontal: 24,
-      marginBottom: 24,
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'space-around',
+        marginHorizontal: 24,
+        marginVertical: 24,
+    },
+    header: {
+        height: 40,
+        flexDirection: 'row',
+    },
+    section: {
+        flex: 1,
+        marginBottom: 24,
+    },
+    footer: {
+        height: 150,
+        justifyContent: 'flex-end',
+    },
+
+    /*==============================================================================*/
+    /* Text styling.
+    /*==============================================================================*/
+    pNor: {
+        fontSize: 18,
+    },
+    pMed: {
+        fontSize: 18,
+        fontWeight: '500',
+    },
+    h1Sem: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+
+    /*==============================================================================*/
+    /* Section.
+    /*==============================================================================*/
+    // Header detail
+    detail: {
+        flexDirection: 'row',
+        height: 54,
+        justifyContent: 'center',
+        marginHorizontal: 16,
+    },
+    leftDetail: {
+        width: 70,
+    },
+    centerDetail: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    rightDetail: {
+        width: 70,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+
+    // Dzikr card
+    card: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 20,
+        paddingHorizontal: 24,
+        paddingVertical: 36,
+        borderRadius: 30,
+        backgroundColor: '#f2f2f7',
+    },
+    cardLabel: {
+        color: '#8E8E93',
+        fontSize: 18,
+        fontWeight: '500',
+    },
+    cardArabic: {
+        fontSize: 24,
+        fontWeight: '500',
+        textAlign: 'center',
+        lineHeight: 40,
+        marginBottom: 16,
+        writingDirection: 'rtl',
+    },
+
+    /*==============================================================================*/
+    /* Footer.
+    /*==============================================================================*/
+    // Footer Container
+    actionContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    moreActionContainer: {
+        flexDirection: 'row',
+        marginBottom: 15,
+        justifyContent: 'space-between',
+    },
+    // Footer Button
+    prevNextAction: {
+        flex: .7,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    centerAction: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    subtractAction: {
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        marginRight: 35,
+    },
+    resetAction: {
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
     },
 }));

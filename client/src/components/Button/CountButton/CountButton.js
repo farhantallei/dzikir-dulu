aae255e038980601cfa943676d2769a957906a2d
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-14 14:25:37 +0700

    remove useEffects and create disabled property

--- client/src/components/Button/CountButton/CountButton.js
+++ client/src/components/Button/CountButton/CountButton.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -7,20 +7,15 @@ import useStyles from './styles.js';
 const CountButton = props => {
     const styles = useStyles(props);
     const [press, setPress] = useState(false);
-    const [topLengthRadius, setTopLengthRadius] = useState(0);
-    
-    useEffect(() => {
-        const width = props.width;
-        const length = props.length;
-        const getBorderTopRadius = `${(((width/2)-length)/(width/2))/2*100}%`;
 
-        setTopLengthRadius(getBorderTopRadius);
-    }, [topLengthRadius])
-
+    const disabled  = props.disabled;
+    const width = props.width;
+    const length = props.length;
+    const getBorderTopRadius = `${(((width/2)-length)/(width/2))/2*100}%`;
     
     const setTopRadius = {
-        borderTopLeftRadius: topLengthRadius,
-        borderTopRightRadius: topLengthRadius,
+        borderTopLeftRadius: getBorderTopRadius,
+        borderTopRightRadius: getBorderTopRadius,
     }
 
     const onPressIn = () => {
@@ -32,9 +27,9 @@ const CountButton = props => {
     };
 
     return (
-        <TouchableWithoutFeedback onPress={props.onPress} onPressIn={onPressIn} onPressOut={onPressOut}>
-            <View style={ press ? { ...styles.button, ...setTopRadius, ...styles.buttonPress } : { ...styles.button, ...setTopRadius } }>
-                <View style={ press ? { ...styles.height, ...styles.heightPress } : styles.height }>
+        <TouchableWithoutFeedback disabled={props.disabled} onPress={props.onPress} onPressIn={onPressIn} onPressOut={onPressOut}>
+            <View style={ disabled ? { ...styles.button, ...styles.buttonPress } : (press ? { ...styles.button, ...setTopRadius, ...styles.buttonPress } : { ...styles.button, ...setTopRadius }) }>
+                <View style={ disabled ? { ...styles.height, ...styles.heightPress } :(press ? { ...styles.height, ...styles.heightPress } : styles.height) }>
                     <View style={styles.inner}>
                         <Text style={styles.label}>{props.count}</Text>
                     </View>
@@ -45,7 +40,7 @@ const CountButton = props => {
 }
 
 CountButton.propTypes = {
-    count: PropTypes.number,
+    disabled: PropTypes.bool,
     color: PropTypes.string,
     backgroundColor: PropTypes.string,
     width: PropTypes.number,
@@ -56,12 +51,13 @@ CountButton.propTypes = {
 }
 
 CountButton.defaultProps = {
+    disabled: false,
     count: 0,
     color: '#ffffff',
     backgroundColor: '#007aff',
     width: 100,
     length: 6,
-    fontWeight: 'bold',
+    fontWeight: '800',
     fontSize: 36,
     textTransform: 'uppercase',
 }

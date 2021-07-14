bcbb8673e85eb76afbbfff91ee7e85cd59cb6430
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-14 15:59:33 +0700

    uninstall body-parser module

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
@@ -7,13 +6,13 @@ import collectionRoutes from './routes/collections.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/collections', collectionRoutes);
 
-const CONNECTION_URL = 'mongodb://farhantallei:419555444Sifrhn@sakudzikr-cluster-0-shard-00-00.mmgd6.mongodb.net:27017,sakudzikr-cluster-0-shard-00-01.mmgd6.mongodb.net:27017,sakudzikr-cluster-0-shard-00-02.mmgd6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-irhbzc-shard-0&authSource=admin&retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb://farhantallei:419555444@dzikir-data-cluster-shard-00-00.mmgd6.mongodb.net:27017,dzikir-data-cluster-shard-00-01.mmgd6.mongodb.net:27017,dzikir-data-cluster-shard-00-02.mmgd6.mongodb.net:27017/dzikir-database?ssl=true&replicaSet=atlas-xsawxy-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })

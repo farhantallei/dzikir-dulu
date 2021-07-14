a7b0235e1b97cdcbc863f4a4f0f8c3d055e95111
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-14 14:28:33 +0700

    remove autoScaling text function and create multiple properties

--- client/src/components/Cards/Card/Card.js
+++ client/src/components/Cards/Card/Card.js
@@ -6,32 +6,7 @@ import useStyles from './styles.js';
 
 const Card = props => {
     const styles = useStyles(props);
-    const [size, setSize] = useState(6);
-    const [status, setStatus] = useState({ opacity: 0, animating: true });
-    const [scale, setScale] = useState({ fontSize: 1, lineHeight: 1.6666666666666667 })
-    const [cardHeight, setCardHeight] = useState();
-    const [titleHeight, setTitleHeight] = useState();
-
-    useEffect(() => {
-        setScale({ fontSize: size, lineHeight: size*1.6666666666666667 });
-    }, [size])
-
-    const getHeight = e => {
-        const { height } = e.nativeEvent.layout;
-
-        if (height >= (cardHeight-titleHeight-24) || size >= 24) {
-            setStatus({ opacity: 1, animating: false });
-            console.log('a ' + height);
-            console.log(cardHeight-titleHeight-24);
-            return;
-        }
-
-        if (size < 24) {
-            setSize(state => state + .5);
-            console.log('b ' + height);
-            return getHeight;
-        }
-    };
+    const [status, setStatus] = useState({ opacity: 1, animating: false });
 
     return (
         <View style={styles.container}>
@@ -39,28 +14,35 @@ const Card = props => {
                 <View style={styles.loadingContainer}>
                     <ActivityIndicator animating={status.animating} />
                 </View>
-                <View style={{ ...styles.content, opacity: status.opacity }} onLayout={e => setCardHeight(e.nativeEvent.layout.height)}>
+                <View style={{ ...styles.content, opacity: status.opacity }}>
                     <View style={styles.arabicContainer}>
-                        <Text style={{ ...styles.arabic, ...scale }} onLayout={getHeight}>
-                            <Text>قُلْ أَعُوذُ بِرَبِّ النَّاسِ، مَلِكِ النَّاسِ، إِلَهِ النَّاسِ، مِن شَرِّ الْوَسْوَاسِ الْخَنَّاسِ، الَّذِي يُوَسْوِسُ فِي صُدُورِ النَّاسِ، مِنَ الْجِنَّةِ وَ النَّاسِ</Text>
-                        </Text>
+                        <Text style={styles.arabic}>{props.arabic}</Text>
                     </View>
-                    <View style={styles.titleContainer} onLayout={e => setTitleHeight(e.nativeEvent.layout.height)}>
-                        <Text style={styles.title}>(QS. An-Naas: 1-6)</Text>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>{props.title}</Text>
                     </View>
                 </View>
             </View>
-            <Text style={{ ...styles.source, opacity: status.opacity }}>HR. Muslim no. 2723</Text>
+            <View style={{ paddingHorizontal: 24, minHeight: 51 }}>
+                <Text style={{ ...styles.source, opacity: status.opacity }}>{props.source}</Text>
+            </View>
         </View>
     );
 }
 
 Card.propTypes = {
-    
+    arabic: PropTypes.string,
+    title: PropTypes.string,
+    source: PropTypes.string,
+    timesTotal: PropTypes.number,
 }
 
 Card.defaultProps = {
-    
+    arabic: 'بِسْمِ الله',
+    title: '',
+    source: 'Source',
+    timesTotal: 1,
+    backgroundColor: '#f2f2f7',
 }
 
 export default Card;

31fc2516bd5bda4e84ffbb8297419a7e4d07237d
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-15 13:28:07 +0700

    remove console log

--- server/controllers/collections.js
+++ server/controllers/collections.js
@@ -3,7 +3,6 @@ import Collection from '../models/Collection.js';
 export const getCollection = async (req, res) => {
     try {
         const collections = await Collection.find();
-        console.log(collections);
 
         res.status(200).json(collections);
     } catch (error) {

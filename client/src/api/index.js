d51fb78d35fc1dd8b4169d5e7c522f6672d7f7e5
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-17 17:50:09 +0700

    update url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const pagiCollectionURL = 'http://localhost:5000/collections/pagi';
-const petangCollectionURL = 'http://localhost:5000/collections/petang';
+const pagiCollectionURL = 'https://dzikir-dulu.herokuapp.com/collections/pagi';
+const petangCollectionURL = 'https://dzikir-dulu.herokuapp.com/collections/petang';
 
 export const fetchPagiCollections = () => axios.get(pagiCollectionURL);
 export const fetchPetangCollections = () => axios.get(petangCollectionURL);

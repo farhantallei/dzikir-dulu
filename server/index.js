7a5ba9eb32d027dcd24c37e79482e446efd6d9c3
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2021-07-17 17:25:16 +0700

    create env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import collectionRoutes from './routes/collections.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
@@ -12,11 +14,9 @@ app.use(cors());
 
 app.use('/collections', collectionRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://farhantallei:419555444Sifrhn@dzikir-data-cluster.mmgd6.mongodb.net/dzikir-database?retryWrites=true&w=majority';
-const CONNECTION_URL = 'mongodb://farhantallei:419555444Sifrhn@dzikir-data-cluster-shard-00-00.mmgd6.mongodb.net:27017,dzikir-data-cluster-shard-00-01.mmgd6.mongodb.net:27017,dzikir-data-cluster-shard-00-02.mmgd6.mongodb.net:27017/dzikir-database?ssl=true&replicaSet=atlas-xsawxy-shard-0&authSource=admin&retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
